e106011aeeacbc6f2960638571580e9fc49c48af
Олександр Owl <aischenko99@gmail.com>
2020-10-01 01:25:10 +0300

    no message

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+console.log(document.querySelector("h1").textContent);
+var img = document.getElementById("img1");
+console.log(img);
+console.log(" Width - " + img.naturalWidth, "Height - " + img.naturalHeight);
+console.log();
+console.log("All h2 elemets text");
+console.log(document.querySelectorAll("h2").forEach(element => console.log(element.textContent)));
+var price = document.querySelectorAll(".price p");
+var tariff = document.querySelectorAll(".price h3");
+console.log(price);
+console.log(tariff);
+for (let index = 0; index < price.length; index++) {    
+    console.log(tariff[index].textContent, "Price - " + price[index].textContent);
+}
+console.log("Text count -"+document.querySelectorAll(".text").length);
